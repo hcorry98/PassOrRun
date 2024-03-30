37a7884c3d81a4e7cc7ea4a9e8755224dee4a6ef
hcorry98 <102935249+hcorry98@users.noreply.github.com>
2024-03-30 15:42:22 -0600

    Center on ball and provide point differential

--- CleanData.py
+++ CleanData.py
@@ -7,39 +7,40 @@ class CleanData:
     DIRTY_PATH = 'data/dirty/nfl-big-data-bowl-20'
     CLEAN_PATH = 'data/clean/20'
 
+    def __init__(self):
+        self.ballDict = {}
+
     def findFiles(self):
         self.files = {}
         self.files[23] = glob.glob(f'{self.DIRTY_PATH}23/**/*.csv', recursive=True)
         self.files[24] = glob.glob(f'{self.DIRTY_PATH}24/**/*.csv', recursive=True)
 
-    def __cleanPlays(self, year):
-        plays = pd.read_csv(f'{self.DIRTY_PATH}{year}/plays.csv')
-        plays = plays[[
-            'gameId', 'playId', 'quarter', 'down', 'yardsToGo', 'possessionTeam', 'gameClock',
-            'preSnapHomeScore', 'preSnapVisitorScore', 'absoluteYardlineNumber',
-            'offenseFormation', 'passResult'
-        ]]
+    def _buildBallDict(self, df):
+        self.ballDict = {}
+        ballDf = df.copy()
 
-        # TODO: change preSnapScores to point differential
+        ballDf = ballDf.loc[ballDf['team'] == 'football']
+        ballDf['uniqueId'] = ballDf['gameId'].astype(str) + '_' + ballDf['playId'].astype(str)
 
-        plays['year'] = 2000 + year
+        ballDf.apply(self.__buildDict, axis=1)
 
-        plays['playType'] = 'pass'
-        plays.loc[plays['passResult'].isnull(), 'playType'] = 'run'
-        plays = plays.drop(columns=['passResult'])
+    def __buildDict(self, row):
+        self.ballDict[row['uniqueId']] = (row['x'], row['y'])
+    
+    def _centerPlayers(self, df):
+        df['uniqueId'] = df['gameId'].astype(str) + '_' + df['playId'].astype(str)
+        df = df.apply(self.__centerDf, axis=1)
+        return df
+    
+    def __centerDf(self, row):
+        row['x'] = row['x'] - self.ballDict[row['uniqueId']][0]
+        row['y'] = row['y'] - self.ballDict[row['uniqueId']][1]
+        return row
 
-        return plays
-
-    def __cleanPlayers(self, year):
-        players = pd.read_csv(f'{self.DIRTY_PATH}{year}/players.csv')
-        players = players.drop(columns=['height', 'weight', 'birthDate', 'collegeName'])
-        if 'officialPosition' in players.columns:
-            players = players.rename(columns={'officialPosition': 'position'})
-        return players
-
-    def __cleanTracking(self, file):
+    def _cleanTracking(self, file):
         df = pd.read_csv(file)
-        df = df.loc[df['event'] == 'ball_snap']
+        snapEvents = ['ball_snap', 'autoevent_ballsnap']
+        df = df.loc[df['event'].isin(snapEvents)]
 
         colsToDrop = ['frameId', 'time', 'jerseyNumber', 'event']
         if 'displayName' in df.columns:
@@ -49,9 +50,50 @@ class CleanData:
         if 'club' in df.columns:
             df = df.rename(columns={'club': 'team'})
 
-        # TODO: center coordinates on ball
-        
+        self._buildBallDict(df)
+        df = self._centerPlayers(df)
+
         return df
+
+    def _cleanPlayers(self, year):
+        players = pd.read_csv(f'{self.DIRTY_PATH}{year}/players.csv')
+        players = players.drop(columns=['height', 'weight', 'birthDate', 'collegeName'])
+        if 'officialPosition' in players.columns:
+            players = players.rename(columns={'officialPosition': 'position'})
+        return players
+
+
+    def _cleanPlays(self, year):
+        plays = pd.read_csv(f'{self.DIRTY_PATH}{year}/plays.csv')
+        plays = plays[[
+            'gameId', 'playId', 'quarter', 'down', 'yardsToGo', 'possessionTeam', 'gameClock',
+            'preSnapHomeScore', 'preSnapVisitorScore', 'absoluteYardlineNumber',
+            'offenseFormation', 'passResult'
+        ]]
+
+        plays['year'] = 2000 + year
+
+        plays['playType'] = 'pass'
+        plays.loc[plays['passResult'].isnull(), 'playType'] = 'run'
+        plays = plays.drop(columns=['passResult'])
+
+        return plays
+
+    def __calcWinningBy(self, row):
+        if row['homeTeamAbbr'] == row['team']:
+            return row['preSnapHomeScore'] - row['preSnapVisitorScore']
+        else:
+            return row['preSnapVisitorScore'] - row['preSnapHomeScore']
+
+    def _getWinningBy(self, master, year):
+        games = pd.read_csv(f'{self.DIRTY_PATH}{year}/games.csv')
+        games = games[['gameId', 'homeTeamAbbr', 'visitorTeamAbbr']]
+
+        master = pd.merge(master, games, how='left', on='gameId')
+        master['winningBy'] = master.apply(self.__calcWinningBy, axis=1)
+
+        master = master.drop(columns=['homeTeamAbbr', 'visitorTeamAbbr', 'preSnapHomeScore', 'preSnapVisitorScore'])
+        return master
 
     def clean(self):
         for year, files in self.files.items():
@@ -60,24 +102,26 @@ class CleanData:
             master = pd.DataFrame()
             for file in files:
                 if file[-5].isdigit():
-                    df = self.__cleanTracking(file)
+                    df = self._cleanTracking(file)
                     master = pd.concat([master, df])
 
-            players = self.__cleanPlayers(year)
-            plays = self.__cleanPlays(year)
+            players = self._cleanPlayers(year)
+            plays = self._cleanPlays(year)
 
-            master = pd.merge(master, players, how='inner', on='nflId')
-            master = pd.merge(master, plays, how='inner', on=['gameId', 'playId'])
+            master = pd.merge(master, players, how='left', on='nflId')
+            master = pd.merge(master, plays, how='left', on=['gameId', 'playId'])
 
             master['offense'] = False
             master.loc[master['team'] == master['possessionTeam'], 'offense'] = True
             master = master.drop(columns=['possessionTeam'])
 
+            master = self._getWinningBy(master, year)
+
             master = master[[
                 'gameId', 'playId', 'offense', 'position', 'displayName', 'team',
                 'x', 'y', 's', 'a', 'dis', 'o', 'dir', 'offenseFormation', 'playDirection',
                 'down', 'yardsToGo', 'absoluteYardlineNumber', 'quarter', 'gameClock',
-                'preSnapHomeScore', 'preSnapVisitorScore', 'year', 'playType'
+                'winningBy', 'year', 'playType'
             ]]
 
             print('Saving cleaned data...')
